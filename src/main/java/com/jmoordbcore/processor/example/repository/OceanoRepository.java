0259081d502652ff2953943e531c2a510af31d80
avbravo <avbravo@avbravo>
2022-07-28 11:44:51 -0500

    LookupAnalizer

--- src/main/java/com/jmoordbcore/processor/example/repository/OceanoRepository.java
+++ src/main/java/com/jmoordbcore/processor/example/repository/OceanoRepository.java
@@ -4,27 +4,12 @@
  */
 package com.jmoordbcore.processor.example.repository;
 
-import com.jmoordb.core.annotation.Delete;
-import com.jmoordb.core.annotation.repository.Count;
-import com.jmoordb.core.annotation.repository.CountRegex;
-import com.jmoordb.core.annotation.repository.Ping;
-import com.jmoordb.core.annotation.repository.Query;
-import com.jmoordb.core.annotation.repository.QueryRegex;
-import com.jmoordb.core.annotation.enumerations.ActivatePagination;
-import com.jmoordb.core.annotation.enumerations.ActivateSort;
-import com.jmoordb.core.pagination.Pagination;
 import java.util.List;
-import java.util.Optional;
-import org.bson.Document;
-import com.jmoordb.core.annotation.repository.QueryJSON;
-import com.jmoordb.core.annotation.enumerations.CaseSensitive;
 import com.jmoordb.core.annotation.enumerations.JakartaSource;
-import com.jmoordb.core.annotation.enumerations.TypeOrder;
 import com.jmoordb.core.annotation.repository.Repository;
-import com.jmoordb.core.annotation.repository.Save;
-import com.jmoordb.core.annotation.repository.Update;
-import com.jmoordb.core.sorted.Sorted;
+import com.jmoordb.core.model.Search;
 import com.jmoordbcore.processor.example.model.Oceano;
+import com.jmoordb.core.annotation.repository.Lookup;
 
 /**
  *
@@ -34,46 +19,45 @@ import com.jmoordbcore.processor.example.model.Oceano;
         database = "{mongodb.database}", collection = "oceano")
 public interface OceanoRepository {
 
-    @Query()
-    public List<Oceano> findAll();
+//    @Query()
+//    public List<Oceano> findAll();
+//
+//    @Query(where = "idoceano .eq. @idoceano")
+//    public Optional<Oceano> findById(String idoceano);
+//
+//    @Query(where = "oceano .eq. @oceano ")
+//    public List<Oceano> findByOceano(String oceano);
+//
+//    @Query(where = "pagination .skip. @pagination")
+//    public List<Oceano> findAllPagination(Pagination pagination);
+//
+//    @Query(where = "sorted .by. @sorted")
+//    public List<Oceano> findAllOrder(Sorted sorted);
+//
+//    @Query(where = "pagination .skip. @pagination .order. sorted .by. @sorted")
+//    public List<Oceano> findAllPaginationSorted(Pagination pagination, Sorted sorted);
+//
+//    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination")
+//    public List<Oceano> findAByIdOceanoPagination(String idoceano, Pagination pagination);
+//
+//    @Query(where = "idoceano .eq. @idoceano .order. sorted .by. @sorted")
+//    public List<Oceano> findAByIdOceanoSorted(String idoceano, Sorted sorted);
+//
+//    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
+//    public List<Oceano> findAByIdOceanoPaginationSorted(String idoceano, Pagination pagination, Sorted sorted);
+//
+//    @Query(where = "idoceano .eq. @idoceano .and. oceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
+//    public List<Oceano> findByIdoceanoAndOceanoPaginationSorted(String idoceano, String oceano, Pagination pagination, Sorted sorted);
+//
+//    @Query(where = "idoceano .eq. @idoceano .and. oceano .eq. @oceano")
+//    public List<Oceano> findByIdoceanoAndOceano(String idoceano, String oceano);
+//
+//    @Query(where = "oceano .eq. @oceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
+//    public List<Oceano> findByOceanoPagination(String oceano, Pagination pagination, Sorted sorted);
+
+    @Lookup
+    public List<Oceano> lookup(Search search);
     
-    @Query(where = "idoceano .eq. @idoceano")
-     public Optional<Oceano> findById(String idoceano);
-
-    @Query(where = "oceano .eq. @oceano ")
-    public List<Oceano> findByOceano(String oceano);
-
-    @Query(where = "pagination .skip. @pagination")
-    public List<Oceano> findAllPagination(Pagination pagination);
-
-    @Query(where = "sorted .by. @sorted")
-    public List<Oceano> findAllOrder(Sorted sorted);
-
-    @Query(where = "pagination .skip. @pagination .order. sorted .by. @sorted")
-    public List<Oceano> findAllPaginationSorted(Pagination pagination, Sorted sorted);
-
-    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination")
-    public List<Oceano> findAByIdOceanoPagination(String idoceano, Pagination pagination);
-    
-    @Query(where = "idoceano .eq. @idoceano .order. sorted .by. @sorted")
-    public List<Oceano> findAByIdOceanoSorted(String idoceano, Sorted sorted);
-    
-  
-    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
-    public List<Oceano> findAByIdOceanoPaginationSorted(String idoceano, Pagination pagination, Sorted sorted);
-
-    @Query(where = "idoceano .eq. @idoceano .and. oceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
-    public List<Oceano> findByIdoceanoAndOceanoPaginationSorted(String idoceano, String oceano, Pagination pagination, Sorted sorted);
-   
-
-    @Query(where = "idoceano .eq. @idoceano .and. oceano .eq. @oceano")
-    public List<Oceano> findByIdoceanoAndOceano(String idoceano, String oceano);
-
-    @Query(where = "oceano .eq. @oceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
-    public List<Oceano> findByOceanoPagination(String oceano, Pagination pagination, Sorted sorted);
-
-//    @QueryJSON(activatePagination = ActivatePagination.ON, activateSort = ActivateSort.ON)
-//    public List<Oceano> queryJSON(Document filter, Pagination pagination, Sorted sorted);
 //
 //    @QueryRegex(field = "oceano", activatePagination = ActivatePagination.ON, caseSensitive = CaseSensitive.NO, typeOrder = TypeOrder.ASC)
 //    public List<Oceano> findRegex(String value, Pagination pagination);
