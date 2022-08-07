214d020d49ada301293e455d9e23bbc47c50f270
avbravo <avbravo@gmail.com>
2022-08-06 20:23:41 -0500

    generar auto

--- src/main/java/com/jmoordbcore/processor/example/repository/OceanoRepository.java
+++ src/main/java/com/jmoordbcore/processor/example/repository/OceanoRepository.java
@@ -4,25 +4,15 @@
  */
 package com.jmoordbcore.processor.example.repository;
 
-import com.jmoordb.core.annotation.enumerations.CaseSensitive;
 import com.jmoordb.core.annotation.enumerations.JakartaSource;
-import com.jmoordb.core.annotation.enumerations.TypeOrder;
-import com.jmoordb.core.annotation.repository.Count;
-import com.jmoordb.core.annotation.repository.Delete;
-import com.jmoordb.core.annotation.repository.Lookup;
-import com.jmoordb.core.annotation.repository.Ping;
 import com.jmoordb.core.annotation.repository.Query;
-import com.jmoordb.core.annotation.repository.Regex;
-import com.jmoordb.core.annotation.repository.RegexCount;
 import com.jmoordb.core.annotation.repository.Repository;
-import com.jmoordb.core.annotation.repository.Save;
 import com.jmoordb.core.annotation.repository.Update;
 import com.jmoordb.core.model.Pagination;
-import com.jmoordb.core.model.Search;
 import com.jmoordb.core.model.Sorted;
 import com.jmoordbcore.processor.example.model.Oceano;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 /**
  *
@@ -32,20 +22,20 @@ import java.util.Optional;
         database = "{mongodb.database}", collection = "oceano")
 public interface OceanoRepository {
 
-//    @Query()
-//    public List<Oceano> findAll();
-//
-//    @Query()
-//    public Set<Oceano> findAllSet();
+    @Query()
+    public List<Oceano> findAll();
+
+    @Query()
+    public Set<Oceano> findAllSet();
 //
 //    @Query(where = "idoceano .eq. @idoceano")
 //    public Optional<Oceano> findById(String idoceano);
 //
-//    @Query(where = "oceano .eq. @oceano ")
-//    public List<Oceano> findByOceano(String oceano);
+    @Query(where = "oceano .eq. @oceano ")
+    public List<Oceano> findByOceano(String oceano);
 //
-//    @Query(where = "pagination .skip. @pagination")
-//    public List<Oceano> findAllPagination(Pagination pagination);
+    @Query(where = "pagination .skip. @pagination")
+    public List<Oceano> findAllPagination(Pagination pagination);
 //
 //    @Query(where = "sorted .by. @sorted")
 //    public List<Oceano> findAllOrder(Sorted sorted);
@@ -59,9 +49,9 @@ public interface OceanoRepository {
 //    @Query(where = "idoceano .eq. @idoceano .order. sorted .by. @sorted")
 //    public List<Oceano> findAByIdOceanoSorted(String idoceano, Sorted sorted);
 //
-//    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
-//    public List<Oceano> findAByIdOceanoPaginationSorted(String idoceano, Pagination pagination, Sorted sorted);
-//
+    @Query(where = "idoceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
+    public List<Oceano> findAByIdOceanoPaginationSorted(String idoceano, Pagination pagination, Sorted sorted);
+
 //    @Query(where = "idoceano .eq. @idoceano .and. oceano .eq. @idoceano .limit. pagination .skip. @pagination .order. sorted .by. @sorted")
 //    public List<Oceano> findByIdoceanoAndOceanoPaginationSorted(String idoceano, String oceano, Pagination pagination, Sorted sorted);
 //
@@ -73,6 +63,8 @@ public interface OceanoRepository {
 //
 //    @Lookup
 //    public List<Oceano> lookup(Search search);
+//    @Lookup
+//    public Set<Oceano> lookupSet(Search search);
 //
 //    @Regex(where = "oceano .like. @oceano  ", caseSensitive = CaseSensitive.NO, typeOrder = TypeOrder.ASC)
 //    public List<Oceano> regex(String oceano);
@@ -93,8 +85,8 @@ public interface OceanoRepository {
     @Update
     public Boolean update(Oceano oceano);
 
-    @Save
-    public Optional<Oceano> save(Oceano oceano);
+//    @Save
+//    public Optional<Oceano> save(Oceano oceano);
 //
 //    @Save
 //    public Boolean saveOceano(Oceano oceano);
